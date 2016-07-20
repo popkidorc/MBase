663e594ddb5128e566532f7ed5269445c424f865
popkidorc <pop.kid.orc@163.com>
2016-07-20 13:37:54 +0800

    init

---
+++ MBase/MBase/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MBase
+//
+//  Created by sunjie on 16/7/20.
+//  Copyright © 2016年 popkidorc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
