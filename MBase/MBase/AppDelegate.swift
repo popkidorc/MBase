14cc2598f76615fe993e583812021948cfc07f20
popkidorc <pop.kid.orc@163.com>
2016-07-21 15:51:33 +0800

    v0.2

--- MBase/MBase/AppDelegate.swift
+++ MBase/MBase/AppDelegate.swift
@@ -16,13 +16,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var documentViewController: DocumentViewController!;
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        // 1. 创建documentViewController
         documentViewController = DocumentViewController(nibName: "DocumentViewController", bundle: nil);
         documentViewController.setupSampleBugs();
         
+        // 2. 添加view
         let contentView = window.contentView!;
         contentView.addSubview(documentViewController.view);
         documentViewController.view.frame = contentView.bounds;
+        
+        
+        // 3. 设置masterViewController.view的布局约束
+        documentViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|[subView]|",
+                                                                                 options: NSLayoutFormatOptions(rawValue: 0),
+                                                                                 metrics: nil,
+                                                                                 views: ["subView" : documentViewController.view]);
+        let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[subView]|",
+                                                                                   options: NSLayoutFormatOptions(rawValue: 0),
+                                                                                   metrics: nil,
+                                                                                   views: ["subView" : documentViewController.view]);
+        NSLayoutConstraint.activateConstraints(verticalConstraints + horizontalConstraints)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
--- MBase/MBase/DocumentViewController.swift
+++ MBase/MBase/DocumentViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import Cocoa
+import Quartz
 
 class DocumentViewController: NSViewController {
-
+    
     @IBOutlet weak var bugsTableView: NSTableView!
     
     @IBOutlet weak var bugTitleView: NSTextField!
@@ -17,6 +18,14 @@ class DocumentViewController: NSViewController {
     @IBOutlet weak var bugRating: EDStarRating!
     
     @IBOutlet weak var bugImageView: NSImageView!
+    
+    @IBOutlet weak var deleteButton: NSButton!
+    
+    @IBOutlet weak var changePicButton: NSButton!
+    
+    @IBOutlet weak var nameLabel: NSTextField!
+    
+    @IBOutlet weak var ratingLabel: NSTextField!
     
     var bugs = [ScaryBugDoc]();
     
@@ -36,46 +45,6 @@ class DocumentViewController: NSViewController {
                                thumbImage:NSImage(named: "ladybugThumb"), fullImage:NSImage(named: "ladybug"))
         bugs = [bug1, bug2, bug3, bug4]
     }
-    
-    
-    override func loadView() {
-        super.loadView()
-        self.bugRating.starHighlightedImage = NSImage(named:"shockedface2_full")
-        self.bugRating.starImage = NSImage(named:"shockedface2_empty")
-        self.bugRating.delegate = self as EDStarRatingProtocol;
-        self.bugRating.maxRating = 5
-        self.bugRating.horizontalMargin = 12
-        self.bugRating.editable = true
-        self.bugRating.displayMode = UInt(EDStarRatingDisplayFull)
-        self.bugRating.rating = Float(0.0)
-    }
-    
-}
-
-// MARK: - NSTableViewDataSource
-extension DocumentViewController: NSTableViewDataSource {
-    
-    func numberOfRowsInTableView(aTableView: NSTableView) -> Int {
-        return self.bugs.count;
-    }
-
-    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        // 1
-        let cellView: NSTableCellView = tableView.makeViewWithIdentifier(tableColumn!.identifier, owner: self) as! NSTableCellView;
-        // 2
-        if tableColumn!.identifier == "BugColumn" {
-            // 3
-            let bugDoc = self.bugs[row]
-            cellView.imageView!.image = bugDoc.thumbImage
-            cellView.textField!.stringValue = bugDoc.data.title
-            return cellView
-        }
-        return cellView
-    }
-}
-
-// MARK: - NSTableViewDelegate
-extension DocumentViewController: NSTableViewDelegate {
     
     func selectedBugDoc() -> ScaryBugDoc? {
         let selectedRow = self.bugsTableView.selectedRow;
@@ -99,14 +68,162 @@ extension DocumentViewController: NSTableViewDelegate {
         self.bugRating.rating = Float(rating)
     }
     
-    func tableViewSelectionDidChange(notification: NSNotification) {
-        let selectedDoc = selectedBugDoc()
-        updateDetailInfo(selectedDoc)
+    
+    func reloadSelectedBugRow() {
+        let indexSet = NSIndexSet(index: self.bugsTableView.selectedRow);
+        let columnSet = NSIndexSet(index: 0);
+        self.bugsTableView.reloadDataForRowIndexes(indexSet, columnIndexes:columnSet);
     }
+    
+    override func loadView() {
+        super.loadView()
+        self.bugRating.starHighlightedImage = NSImage(named:"shockedface2_full")
+        self.bugRating.starImage = NSImage(named:"shockedface2_empty")
+        self.bugRating.delegate = self as EDStarRatingProtocol;
+        self.bugRating.maxRating = 5
+        self.bugRating.horizontalMargin = 12
+        self.bugRating.editable = false
+        self.bugRating.displayMode = UInt(EDStarRatingDisplayFull)
+        self.bugRating.rating = Float(0.0)
+    }
+    
+}
+
+// MARK: - NSTableViewDataSource
+extension DocumentViewController: NSTableViewDataSource {
+    
+    func numberOfRowsInTableView(aTableView: NSTableView) -> Int {
+        return self.bugs.count;
+    }
+    
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        // 1
+        let cellView: NSTableCellView = tableView.makeViewWithIdentifier(tableColumn!.identifier, owner: self) as! NSTableCellView;
+        // 2
+        if tableColumn!.identifier == "BugColumn" {
+            // 3
+            let bugDoc = self.bugs[row]
+            cellView.imageView!.image = bugDoc.thumbImage
+            cellView.textField!.stringValue = bugDoc.data.title
+            return cellView
+        }
+        return cellView
+    }
+}
+
+// MARK: - NSTableViewDelegate
+extension DocumentViewController: NSTableViewDelegate {
+    
+    func tableViewSelectionDidChange(notification: NSNotification) {
+        let selectedDoc = selectedBugDoc();
+        updateDetailInfo(selectedDoc);
+        let enabled = (selectedDoc != nil);
+        deleteButton.enabled = enabled;
+        changePicButton.enabled = enabled;
+        bugRating.editable = enabled;
+        bugTitleView.enabled = enabled;
+        let hidden = (selectedDoc == nil);
+        nameLabel.hidden = hidden;
+        bugTitleView.hidden = hidden;
+        ratingLabel.hidden = hidden;
+        bugRating.hidden = hidden;
+        bugImageView.hidden = hidden;
+        changePicButton.hidden = hidden;
+    }
+    
 }
 
 // MARK: -EDStarRatingProtocol
 extension DocumentViewController: EDStarRatingProtocol {
-
+    
+    func starsSelectionChanged(control: EDStarRating!, rating: Float) {
+        if let selectedDoc = selectedBugDoc() {
+            selectedDoc.data.rating = Double(control.rating)
+        }
+    }
 }
+
+
+// MARK: - IBActions
+extension DocumentViewController {
+    
+    @IBAction func bugTitleDidEndEdit(sender: AnyObject) {
+        if let selectedDoc = selectedBugDoc() {
+            selectedDoc.data.title = self.bugTitleView.stringValue;
+            reloadSelectedBugRow();
+        }
+    }
+    
+    @IBAction func changePicture(sender: AnyObject) {
+        if (selectedBugDoc() != nil) {
+            IKPictureTaker().beginPictureTakerSheetForWindow(self.view.window, withDelegate: self, didEndSelector: #selector(DocumentViewController.pictureTakerDidEnd(_:returnCode:contextInfo:)), contextInfo: nil);
+        }
+    }
+    
+    func pictureTakerDidEnd(picker: IKPictureTaker, returnCode: NSInteger, contextInfo: UnsafePointer<Void>) {
+        let image = picker.outputImage()
+        if image != nil && returnCode == NSModalResponseOK {
+            self.bugImageView.image = image;
+            if let selectedDoc = selectedBugDoc() {
+                selectedDoc.fullImage = image;
+                selectedDoc.thumbImage = image.imageByScalingAndCroppingForSize(CGSize(width: 44, height: 44));
+                reloadSelectedBugRow();
+            }
+        }
+    }
+    
+    @IBAction func resetData(sender: AnyObject) {
+        setupSampleBugs();
+        updateDetailInfo(nil);
+        bugsTableView.reloadData();
+    }
+    
+    @IBAction func addBug(sender: AnyObject) {
+        // 1. 使用默认值创建一个新的ScaryBugDoc实例
+        let newDoc = ScaryBugDoc(title: "New Bug", rating: 0.0, thumbImage: nil, fullImage: nil);
+        
+        // 2. 将该实例添加到model 数组
+        self.bugs.append(newDoc);
+        
+        let newRowIndex = self.bugs.count - 1;
+        
+        // 3.向table view插入新行
+        self.bugsTableView.insertRowsAtIndexes(NSIndexSet(index: newRowIndex), withAnimation: NSTableViewAnimationOptions.EffectGap);
+        
+        // 4. 选中并滚动到新行
+        self.bugsTableView.selectRowIndexes(NSIndexSet(index: newRowIndex), byExtendingSelection:false);
+        self.bugsTableView.scrollRowToVisible(newRowIndex);
+    }
+    
+    
+    @IBAction func deleteBug(sender: AnyObject) {
+        
+        // 1. Get selected doc
+        if (selectedBugDoc() != nil) {
+            
+            // 2. Remove the bug from the model
+            let nowRow = self.bugsTableView.selectedRow;
+            self.bugs.removeAtIndex(nowRow)
+            
+            // 3. Remove the selected row from the table view
+            self.bugsTableView.removeRowsAtIndexes(NSIndexSet(index:self.bugsTableView.selectedRow),withAnimation: NSTableViewAnimationOptions.SlideRight);
+            
+            
+            // 4. 清理model
+            updateDetailInfo(nil);
+            
+            // 5. 选中下一行并滚动到新行
+            var newSelectedRow = nowRow;
+            if(self.bugsTableView.numberOfRows <= nowRow){
+                newSelectedRow = self.bugsTableView.numberOfRows - 1;
+            }
+            self.bugsTableView.selectRowIndexes(NSIndexSet(index: newSelectedRow), byExtendingSelection:false);
+            self.bugsTableView.scrollRowToVisible(newSelectedRow);
+            
+        }
+    }
+}
+
+
+
 
